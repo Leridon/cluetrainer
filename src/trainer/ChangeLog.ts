918cb19a24128d5b20bef889688abf849d9b4205
Lukas Gail <lukas.gail@outlook.com>
2025-01-05 19:11:28 +0100

    Fix missing methods for tetracompass spots

--- src/trainer/ChangeLog.ts
+++ src/trainer/ChangeLog.ts
@@ -31,6 +31,7 @@ export namespace Changelog {
           .item("Added development utilities to simplify editing builtin methods.")
           .item("Added a note that explains how to enable the Canifis to Mort´ton shortcut to a hard clue step.")
           .item("Removed impossible compass spots in the Kharazi jungle.")
+          .item("Fixed missing paths for recently relocated tetracompass spots (v31).")
         )
     }
   }, {
