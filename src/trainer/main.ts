02298d59a681df6d4e332c47f68234e03ffc6241
Lukas Gail <lukas.gail@outlook.com>
2025-01-10 00:43:10 +0100

    Add analysis of full gielinor

--- src/trainer/main.ts
+++ src/trainer/main.ts
@@ -1,12 +1,23 @@
-import {MethodPackManager} from "./model/MethodPackManager";
 import {Path} from "../lib/runescape/pathing";
-import ExportStringModal from "./ui/widgets/modals/ExportStringModal";
-import {util} from "../lib/util/util";
 import {TileArea} from "../lib/runescape/coordinates/TileArea";
 import {ScanTree} from "../lib/cluetheory/scans/ScanTree";
 import * as lodash from "lodash"
-import {MovementAbilities} from "../lib/runescape/movement";
-import cleanedJSON = util.cleanedJSON;
+import {HostedMapData, MovementAbilities} from "../lib/runescape/movement";
+import {angleDifference, Rectangle, Vector2} from "../lib/math";
+import {Compasses} from "../lib/cluetheory/Compasses";
+import {clue_data} from "../data/clues";
+import {Process} from "../lib/Process";
+import {CompassReader} from "./ui/neosolving/cluereader/CompassReader";
+import {util} from "../lib/util/util";
+import {GameLayer} from "../lib/gamemap/GameLayer";
+import {deps} from "./dependencies";
+import {TileMarker} from "../lib/gamemap/TileMarker";
+import {TileCoordinates} from "../lib/runescape/coordinates";
+import {MapEntity} from "../lib/gamemap/MapEntity";
+import Properties from "./ui/widgets/Properties";
+import {GameMapControl} from "../lib/gamemap/GameMapControl";
+import gielinor_compass = clue_data.gielinor_compass;
+import index = util.index;
 
 async function fix_path(p: Path): Promise<number> {
   if (!p) return
@@ -62,38 +73,134 @@ async function fix_tree(tree: ScanTree.ScanTreeNode) {
       : 0)
 }
 
-export async function makeshift_main(): Promise<void> {
+function min_index_by<T>(array: T[], f: (_: T) => number): number {
+  let min_index = -1
+  let min_value = Number.MAX_VALUE
 
-  const packs = [
-    "default:ngiseasy",
-    "default:ngismedium",
-    "default:ngishard",
-    "default:ngiscompass",
-    "default:ngistetra",
-    "default:ngismaster",
-    "default:scanmethods"
-  ]
+  for (let i = 0; i < array.length; i++) {
+    const val = f(array[i])
+    if (val < min_value) {
+      min_index = i
+      min_value = val
+    }
+  }
 
-  for (const pack_id of packs) {
-    const pack = await MethodPackManager.instance().getPack(pack_id)
+  return min_index
+}
 
-    let removed = 0
+function entity(spot: Vector2,
+                value: number) {
+  return new class extends MapEntity {
+    constructor() {
+      super();
 
-    for (let m of pack.methods) {
-      switch (m.type) {
-        case "general_path": {
-          removed += await fix_path(m.pre_path)
-          removed += await fix_path(m.main_path)
-          removed += await fix_path(m.post_path)
-          break;
-        }
-        case "scantree":
-          removed += await fix_tree(m.tree.root)
-      }
+      this.setTooltip(() => {
+        const props = new Properties()
+
+        props.named("Spot", Vector2.toString(spot))
+        props.named("Value", `${value.toFixed(2)} bit`)
+
+        return props
+      })
     }
 
-    await ExportStringModal.do(cleanedJSON(pack), `Removed ${removed} icons`,
-      `${pack.name}.json`
-    )
+    bounds(): Rectangle {
+      return Rectangle.from(spot)
+    }
+
+    protected async render_implementation(props: MapEntity.RenderProps): Promise<Element> {
+      const marker = new TileMarker(TileCoordinates.lift(spot, 0))
+        .withMarker()
+        .addTo(this)
+
+      return marker.marker.getElement()
+    }
   }
+}
+
+export async function makeshift_main(): Promise<void> {
+
+
+  const area: TileArea = {
+    origin: {"x": 2048, "y": 2496, "level": 0},
+    size: {x: 28 * 64, y: 26 * 64}
+  }
+
+  const results: {
+    spot: Vector2,
+    value: number,
+    marker?: MapEntity
+  }[] = new Array(50).fill(null).map(() => ({spot: null, value: 0}))
+
+
+  function informationValue(position: Vector2): number {
+
+    let information = 0
+
+    const angles = gielinor_compass.spots.map(s => Compasses.getExpectedAngle(position, s))
+
+    angles.sort()
+
+    for (let i = 0; i < angles.length; i++) {
+      const angle = angles[i]
+
+      let possibilities = 1
+
+      for (let j = i - 1; angleDifference(angle, index(angles, j)) < CompassReader.EPSILON; j--) possibilities++
+      for (let j = i + 1; angleDifference(angle, index(angles, j)) < CompassReader.EPSILON; j++) possibilities++
+
+      information += Math.log2(gielinor_compass.spots.length / possibilities)
+    }
+
+    return information / gielinor_compass.spots.length
+  }
+
+  const total = area.size.x * area.size.y
+  let progress = 0
+
+  let worst = 0
+
+  const map = HostedMapData.get()
+
+  const status = c();
+
+  deps().app.map.addControl(new GameMapControl({no_default_styling: false, position: "top-center", type: "gapless"}, status))
+
+  const layer = new GameLayer().addTo(deps().app.map)
+
+  const process = new class extends Process {
+    async implementation(): Promise<void> {
+      this.withInterrupt(50, 1)
+
+      const rect = TileArea.toRect(area)
+
+      for (let x = rect.topleft.x; x < rect.botright.x && !this.should_stop; ++x) {
+        for (let y = rect.botright.y; y < rect.topleft.y; ++y) {
+          progress++
+
+          await this.checkTime()
+
+          const tile: TileCoordinates = {x, y, level: 0}
+
+          if (await map.getTile(tile) == 0) continue
+
+          const value = informationValue(tile)
+
+          if (value > results[worst].value) {
+            const w = results[worst]
+
+            w.marker?.remove()
+
+            results[worst] = {spot: tile, value: value, marker: entity(tile, value).addTo(layer)}
+
+            worst = min_index_by(results, r => r.value)
+          }
+
+          status.text(`${progress}/${total} checked, ${(100 * progress / total).toFixed(1)}%`)
+        }
+      }
+    }
+  }
+
+  await process.run()
 }
