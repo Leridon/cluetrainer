45b5095ff761731e0ac6988696b5e5d6a1cd0a77
Lukas Gail <lukas.gail@outlook.com>
2025-01-17 23:45:23 +0100

    Add click handling for spots when not using a scan tree

--- src/trainer/ui/neosolving/subbehaviours/scans/SimpleScanSolving.ts
+++ src/trainer/ui/neosolving/subbehaviours/scans/SimpleScanSolving.ts
@@ -6,8 +6,12 @@ import {Scans} from "../../../../../lib/runescape/clues/scans";
 import {deps} from "../../../../dependencies";
 import {ScanSolving} from "./ScanSolving";
 import {TileArea} from "../../../../../lib/runescape/coordinates/TileArea";
-import {TileRectangle} from "../../../../../lib/runescape/coordinates";
+import {TileCoordinates, TileRectangle} from "../../../../../lib/runescape/coordinates";
 import {ScanMinimapOverlay} from "./ScanMinimapOverlay";
+import {GameLayer} from "../../../../../lib/gamemap/GameLayer";
+import {GameMapMouseEvent} from "../../../../../lib/gamemap/MapEvents";
+import {ScanEditLayer} from "../../../theorycrafting/scanedit/ScanEditor";
+import digSpotArea = Clues.digSpotArea;
 
 export class SimpleScanSolving extends NeoSolvingSubBehaviour {
   settings: ScanSolving.Settings
@@ -26,14 +30,49 @@ export class SimpleScanSolving extends NeoSolvingSubBehaviour {
     }
   }
 
+  private selected_solution: TileCoordinates = null
+
+  private handling_layer: GameLayer = null
+
   protected begin() {
     this.registerSolution(
       TileArea.fromRect(
         TileRectangle.extend(TileRectangle.from(...this.clue.spots), 1)
       )
     )
+
+    const self = this
+
+    this.parent.layer.scan_layer.marker.add(new class extends GameLayer {
+      eventClick(event: GameMapMouseEvent) {
+        event.onPre(() => {
+          if (event.active_entity instanceof ScanEditLayer.SpotMarker) {
+            event.stopAllPropagation()
+
+            if (TileCoordinates.equals(self.selected_solution, event.active_entity.spot)) {
+              self.selected_solution = null
+
+              self.parent.layer.scan_layer.setActiveCandidates(self.clue.spots)
+
+              self.registerSolution(
+                TileArea.fromRect(
+                  TileRectangle.extend(TileRectangle.from(...self.clue.spots), 1)
+                )
+              )
+            } else {
+              self.selected_solution = event.active_entity.spot
+
+              self.parent.layer.scan_layer.setActiveCandidates([event.active_entity.spot])
+
+              self.registerSolution(digSpotArea(event.active_entity.spot))
+            }
+          }
+        })
+      }
+    })
   }
 
   protected end() {
+    this.handling_layer?.remove()
   }
 }
