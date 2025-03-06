cd1b2a8499f7e6bffb15a26e8f94df8819f83211
Lukas Gail <lukas.gail@outlook.com>
2025-03-06 23:59:41 +0100

    Properly deduplicate click events

--- src/lib/alt1/Alt1MouseTracking.ts
+++ src/lib/alt1/Alt1MouseTracking.ts
@@ -3,22 +3,28 @@ import {ewent, EwentHandler, observe} from "../reactive";
 import {Vector2} from "lib/math/Vector2";
 import {Process} from "../Process";
 import {timeSync} from "../gamemap/GameLayer";
-import {OverlayGeometry} from "./OverlayGeometry";
 import {ScreenRectangle} from "./ScreenRectangle";
+import {Alt1Overlay} from "./overlay/Alt1Overlay";
+import {Alt1OverlayDrawCalls} from "./overlay/Alt1OverlayDrawCalls";
 
 export class Alt1MouseTracking {
   private process: Process
   private position = observe<Vector2>(null).structuralEquality()
   private click = ewent<Vector2>()
 
+  private last_active: number = undefined
+
   constructor() {
 
     this.click.on(pos => {
       console.log(`Active at ${Vector2.toString(pos)}`)
 
-      new OverlayGeometry().withTime(1000)
-        .rect2(ScreenRectangle.centeredOn(pos, 5))
-        .render()
+      Alt1Overlay.oneOff(
+        new Alt1OverlayDrawCalls.GeometryBuilder()
+          .rectangle(ScreenRectangle.centeredOn(pos, 5))
+          .buffer(),
+        1000
+      )
     })
 
     this.startTracking()
@@ -34,7 +40,7 @@ export class Alt1MouseTracking {
       constructor() {super(50);}
 
       tick(): void {
-        timeSync("tick", () => {
+       // timeSync("tick", () => {
           if (self.position.changed.handlerCount() == 0 && self.click.handlerCount() == 0) {
             this.stop()
 
@@ -45,8 +51,14 @@ export class Alt1MouseTracking {
 
           self.position.set(a1lib.getMousePosition())
 
-          if (alt1.rsLastActive < 50) self.click.trigger(a1lib.getMousePosition())
-        })
+          const last_active = alt1.rsLastActive
+          const last_active_absolute = Date.now() - last_active
+          const is_new_event = Math.abs(self.last_active - last_active_absolute) >= 2
+
+          if (is_new_event && last_active < 100) self.click.trigger(self.position.value())
+
+          self.last_active = last_active_absolute
+       // })
       }
     }
 
