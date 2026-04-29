7756368da314eefb4cfcbdd05a4610af1dcdc8a7
Lukas Gail <lukas.gail@outlook.com>
2026-04-29 20:38:21 +0200

    Add logging for alt1gl crashes

--- src/lib/alt1/alt1gl/Alt1GLSession.ts
+++ src/lib/alt1/alt1gl/Alt1GLSession.ts
@@ -1,5 +1,7 @@
 import {Alt1} from "../Alt1";
 import {Alt1GlClient} from "../alt1gllib/ts/util/alt1gltypes";
+import {Log} from "../../util/Log";
+import log = Log.log;
 
 export class Alt1GLSession {
   public session: Alt1GlClient
@@ -10,7 +12,10 @@ export class Alt1GLSession {
 
   private refreshSession() {
     const session = this.alt1.raw.getGlSession()
-    session.on("close", () => this.refreshSession())
+    session.on("close", () => {
+      log().log("Alt1GLSession crashed, refreshing...", "Alt1GL")
+      this.refreshSession()
+    })
 
     this.session = session
   }
