0241312dc5486a091e632b921811ab0eb0114748
Lukas Gail <lukas.gail@outlook.com>
2026-07-08 13:17:56 +0200

    Fix zoom edge case in scans

--- src/cluetrainer/ChangeLog.ts
+++ src/cluetrainer/ChangeLog.ts
@@ -442,6 +442,7 @@ export namespace Changelog {
           .list(l => l
             .item("Fixed precise dig spot location of four hard clues.")
             .item("Fixed broken zoom when a path contains an orientation step.")
+            .item("Fixed an issue that caused the map to zoom unreasonably far out in certain edge conditions in scans.")
           )
 
         builder.release(75, "Fixes and Method Updates", date(2026, 6, 17))
